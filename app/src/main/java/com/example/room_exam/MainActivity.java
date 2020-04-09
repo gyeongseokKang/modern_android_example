d8d788e7d4b21bcb132c7c23f973b12f012d138d
Kangkyeungseok <kyeungseok.kang@gmail.com>
2020-04-09 19:52:41 +0900

    모던 안드로이드 #9 - DataBinding

--- app/src/main/java/com/example/room_exam/MainActivity.java
+++ app/src/main/java/com/example/room_exam/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.room_exam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
@@ -12,32 +13,25 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.room_exam.databinding.ActivityMainBinding;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mTodoEditText;
-    private TextView mResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        mTodoEditText = findViewById(R.id.todo_edit);
-        mResultTextView = findViewById(R.id.result_text);
-
+        binding.setLifecycleOwner(this);
 
         MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+
+        binding.setViewModel(viewModel);
         //UI 갱신
-        viewModel.getAll().observe(this, todos -> {
-            mResultTextView.setText(todos.toString() );
-        });
 
-        //버튼 클릭시 DB에 insert
-        findViewById(R.id.add_button).setOnClickListener(v -> {
-            viewModel.insert(new Todo(mTodoEditText.getText().toString()));
 
-        });
     }
 
 
--- app/src/main/java/com/example/room_exam/MainViewModel.java
+++ app/src/main/java/com/example/room_exam/MainViewModel.java
@@ -12,16 +12,20 @@ import java.util.List;
 
 public class MainViewModel extends AndroidViewModel {
     AppDatabase db;
+    public LiveData<List<Todo>> todos;
+    public String newTodo;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
 
         db = Room.databaseBuilder(application, AppDatabase.class, "todo-db")
                 .build();
+
+        todos = getAll();
     }
 
-    public void insert(Todo todo){
-        new InsertAsyncTask(db.todoDao()).execute(todo);
+    public void insert(String todo){
+        new InsertAsyncTask(db.todoDao()).execute(new Todo(todo));
     }
     public LiveData<List<Todo>> getAll(){
         return db.todoDao().getAll();
