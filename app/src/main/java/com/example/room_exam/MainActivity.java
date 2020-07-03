178bd39855f42a38219575150d843c5d34f4fc41
Kang Gyeong Seok <rkdrudtjr159@gmail.com>
2020-07-04 00:20:05 +0900

    안드로이드 블로그 #UUID 를 만드는 방법에 대하여

--- app/src/main/java/com/example/room_exam/MainActivity.java
+++ app/src/main/java/com/example/room_exam/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.room_exam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Base64;
 import android.util.Log;
 
@@ -19,10 +28,12 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FAnalytics mFAnalytics =null;
+    private FAnalytics mFAnalytics = null;
+    private static final int REQUEST_PHONE_STATE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +51,13 @@ public class MainActivity extends AppCompatActivity {
 
         //firebase 인증
         binding.authBottom.setOnClickListener(v -> {
-            mFAnalytics.sendLogEvent("click_test","id_test","name_test");
+            mFAnalytics.sendLogEvent("click_test", "id_test", "name_test");
             Intent intent = new Intent(this, FirebaseUIActivity.class);
             startActivity(intent);
         });
 
         binding.addButton.setOnClickListener(v -> {
-            mFAnalytics.sendLogEvent("click_test","id_test2","name_test2");
+            mFAnalytics.sendLogEvent("click_test", "id_test2", "name_test2");
         });
 
         binding.btnRxandroid.setOnClickListener(v -> {
@@ -54,10 +65,13 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        checkForPhoneStatePermission();
+        GetDevicesUUID(MainActivity.this);
+
         //getHashKey();
     }
 
-    private void getHashKey(){
+    private void getHashKey() {
         PackageInfo packageInfo = null;
         try {
             packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
@@ -76,6 +90,69 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
             }
         }
+    }
+
+    private String GetDevicesUUID(Context mContext) {
+        final TelephonyManager tm = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        final String tmDevice, tmSerial, androidId;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            //return "Fail to Get UUID";
+        }
+        tmDevice = "" + tm.getDeviceId();
+        tmSerial = "" + tm.getSimSerialNumber();
+        androidId = "" + android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+        UUID deviceUuid = new UUID(androidId.hashCode(), ((long)tmDevice.hashCode() << 32) | tmSerial.hashCode());
+        String deviceId = deviceUuid.toString();
+        Log.d("UUID_generater", deviceId);
+        return deviceId;
+    }
+
+    private void checkForPhoneStatePermission(){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.READ_PHONE_STATE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                        Manifest.permission.READ_PHONE_STATE)) {
+
+                    // Show an explanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                    showPermissionMessage();
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_PHONE_STATE},
+                            REQUEST_PHONE_STATE);
+                }
+            }
+        }else{
+            //... Permission has already been granted, obtain the UUID
+            Log.d("UUID_generater", "deviceId");
+            GetDevicesUUID(MainActivity.this);
+        }
+}
+
+
+    private void showPermissionMessage(){
+        new AlertDialog.Builder(this)
+                .setTitle("Read phone state")
+                .setMessage("This app requires the permission to read phone state to continue")
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.READ_PHONE_STATE},
+                                REQUEST_PHONE_STATE);
+                    }
+                }).create().show();
     }
 
 
